b93cf03948476ca5d9f18c3a2788693792b0a4f5
kitada.haiiro <kitada.haiiro@gmail.com>
2020-03-21 23:06:58 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ if [[ -n "$TOKEN" ]]; then
     GITHUB_TOKEN=$TOKEN
 fi
 
-if [[ -z "$OUTPUT_BRANCH" ]]; then
-    OUTPUT_BRANCH="output"
+if [[ -z "$PREVIEW_BRANCH" ]]; then
+    PREVIEW_BRANCH="preview"
 fi
 
 if [[ -z "$BUILD_DIR" ]]; then
@@ -24,10 +24,38 @@ if [[ -z "$GITHUB_REPOSITORY" ]]; then
     exit 1
 fi
 main() {
-    echo "ok"
-    ls
-    tlmgr --version
-    pandoc test/main.md -o hoge.pdf --pdf-engine=lualatex -V documentclass=ltjsarticle -V luatexjapresetoption=ms
+    echo "Starting print..."
+
+    echo "Fetch"
+    git config --global url."https://".insteadOf git://
+    git config --global url."https://github.com/".insteadOf git@github.com:
+    git submodule update --init --recursive
+
+    version=$(tlmgr --version)
+    remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+    remote_branch=$PREVIEW_BRANCH
+
+    echo "Using $version"
+
+    echo "converting..."
+    cd $BUILD_DIR
+
+    mkdir out
+
+    pandoc test/main.md -o out/hoge.pdf --pdf-engine=lualatex -V documentclass=ltjsarticle -V luatexjapresetoption=ms
+
+    echo "Pushing articats to ${GITHUB_REPOSITORY}:$remote_branch"
+
+    cd out
+    git init
+    git config user.name "GA"
+    git config user.email "actions@users.noreplay.github.com"
+    git add .
+
+    git commit -m "Publishing"
+    git push --force "${remote_repo}" master:${remote_branch}
+
+    echo "All done."
 }
 
 main "$@"
